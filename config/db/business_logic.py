35f123e82b56fcd0ebc87fb34a25e50b9ecb8fe6
Роман Малинков <roman.malinkov@list.ru>
2021-06-13 19:18:34 +0700

    Парсинг файла логов: промежуточный вариант с использованием RAM №2 - fix #2

--- config/db/business_logic.py
+++ config/db/business_logic.py
@@ -131,7 +131,7 @@ class ConnectionsLogWrapper:
                     if raw_data[3] != 'null':
                         users.add(raw_data[3])
 
-                    if raw_data[1] not in waps.keys:
+                    if raw_data[1] not in waps.keys():
                         waps[raw_data[1]] = {}
                         waps[raw_data[1]]['lat'] = raw_data[4].replace('(', '').replace('"', '').strip()
                         waps[raw_data[1]]['lon'] = raw_data[5].replace(')', '').replace('"', '').strip()
